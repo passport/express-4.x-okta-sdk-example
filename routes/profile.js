58311172b89bcefff4278e24eb2c5c3480385bee
Jared Hanson <jaredhanson@gmail.com>
2021-04-02 14:16:00 -0700

    Initial commit.

---
+++ routes/profile.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+//var db = require('../db');
+
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', ensureLoggedIn(), function(req, res, next) {
+  /*
+  db.users.findOne({ id: req.user.id }, function (err, record) {
+    if (err) { return next(err); }
+    res.render('profile', { user: record });
+  });
+  */
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var passport = require('passport');
+var path = require('path');
+//var db = require('./db');
+
+var authRouter = require('./routes/auth');
+var profileRouter = require('./routes/profile');
+//var notificationsRouter = require('./routes/notifications');
+
+
+
+// Create a new Express application.
+var app = express();
+
+require('./boot/auth')();
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.use('/', authRouter);
+app.use('/profile', profileRouter);
+//app.use('/notifications', notificationsRouter);
+
+app.listen(3000);
